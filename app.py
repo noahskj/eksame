2fb45df09ba62060fddf145c060fcac1d11ab1d6
Pezpuck <115011462+noahskj@users.noreply.github.com>
2023-05-06 12:44:20 +0200

    Updated the code because of a github fail...

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import random
+from enemy import Enemy
 
 # Initialize Pygame
 pg.init()
@@ -9,8 +10,8 @@ win_width = 768
 win_height = 504
 
 # Define the camera size
-camera_width = 400
-camera_height = 300
+camera_width = 768
+camera_height = 504
 
 # Create the Pygame window
 win = pg.display.set_mode((win_width, win_height))
@@ -18,27 +19,13 @@ win = pg.display.set_mode((win_width, win_height))
 # Set the window title
 pg.display.set_caption("Lord of the QuarterPoundMac")
 
-#musicussy
-pg.mixer.pre_init(44100, 32, 2, 1024)
-pg.mixer.init()
-pg.mixer.music.load("ring.wav")
-pg.mixer.music.play(1)
-pg.mixer.music.queue("hammer.mp3")
-pg.mixer.music.play(loops = 1)
-
 # Define the player sprite and starting position
 player_img = pg.image.load("player.png")
 player_x = 50
 player_y = 50
 
-# Define the enemy sprite and starting position
-enemy_img = pg.image.load("enemy.png")
-enemy_x = random.randint(0, win_width - enemy_img.get_width())
-enemy_y = random.randint(0, win_height - enemy_img.get_height())
-
 # Define the player's movement speed
 move_speed = 0.1
-enemy_speed = 1
 
 # Define the camera position
 camera_x = player_x - camera_width/2
@@ -48,6 +35,30 @@ camera_y = player_y - camera_height/2
 bg = pg.image.load("bg.png")
 bg_x = 0
 bg_y = 0
+
+# Create a list to hold the enemies and bullets
+enemies = []
+bullets = []
+
+# Define the time interval between shots (in milliseconds)
+SHOT_DELAY = 500
+
+# Define a timer to keep track of the time since the last shot
+shot_timer = 0
+
+#musicussy
+pg.mixer.pre_init(44100, 32, 2, 1024)
+pg.mixer.init()
+pg.mixer.music.load("ring.wav")
+pg.mixer.music.play()
+pg.mixer.music.queue("hammer.mp3")
+pg.mixer.music.play(loops = 1)
+
+# Create x enemy objects and add them to the list
+for i in range(3):
+    enemy_img = pg.image.load("enemy.png")
+    enemy = Enemy(random.randint(0, win_width - enemy_img.get_width()), random.randint(0, win_height - enemy_img.get_height()), enemy_img)
+    enemies.append(enemy)
 
 # Define the game loop
 running = True
@@ -68,54 +79,75 @@ while running:
     if keys[pg.K_DOWN]:
         player_y += move_speed
 
-    # Move the enemy randomly
-    enemy_dir = random.choice(["left", "right", "up", "down"])
-    if enemy_dir == "left":
-        enemy_x -= enemy_speed
-    elif enemy_dir == "right":
-        enemy_x += enemy_speed
-    elif enemy_dir == "up":
-        enemy_y -= enemy_speed
-    elif enemy_dir == "down":
-        enemy_y += enemy_speed
+ # Handle shooting by the player
 
-    # Keep the enemy within the screen bounds
-    if enemy_x < 0:
-        enemy_x = 0
-    elif enemy_x > win_width - enemy_img.get_width():
-        enemy_x = win_width - enemy_img.get_width()
-    if enemy_y < 0:
-        enemy_y = 0
-    elif enemy_y > win_height - enemy_img.get_height():
-        enemy_y = win_height - enemy_img.get_height()
+    if keys[pg.K_z] or keys[pg.K_x]:
+        # Check if enough time has elapsed since the last shot
+        time_since_last_shot = pg.time.get_ticks() - shot_timer
+        if time_since_last_shot >= SHOT_DELAY:
+            # Create a new bullet object and add it to the list of bullets
+            if keys[pg.K_z]:
+                bullet_velocity = (-2, 0) # Shoot left
+            else:
+                bullet_velocity = (2, 0) # Shoot right
+            bullet = {"rect": pg.Rect(player_x + player_img.get_width()/2, player_y + player_img.get_height()/2, 8, 8),
+                    "velocity": bullet_velocity}
+            bullets.append(bullet)
+    
+            # Reset the shot timer
+            shot_timer = pg.time.get_ticks()
+  
+    # Move the bullets
+    for bullet in bullets:
+        bullet["rect"].move_ip(*bullet["velocity"])
 
-    # Check for collision between player and enemy
-    if (player_x + player_img.get_width() > enemy_x and player_x < enemy_x + enemy_img.get_width()
-        and player_y + player_img.get_height() > enemy_y and player_y < enemy_y + enemy_img.get_height()):
-        # Player touched the enemy, do something
-        print("Player touched the enemy!")
+        # Remove the bullet if it goes out of screen
+        if bullet["rect"].top < 0:
+            bullets.remove(bullet)
 
-    # Update the camera position to follow the player
+    # Move the enemies
+    for enemy in enemies:
+        enemy.move()
+
+        # Keep the enemy within the screen bounds
+        if enemy.rect.x < 0:
+            enemy.rect.x = 0
+        elif enemy.rect.x > win_width - enemy.rect.width:
+            enemy.rect.x = win_width - enemy.rect.width
+        if enemy.rect.y < 0:
+            enemy.rect.y = 0
+        elif enemy.rect.y > win_height - enemy.rect.height:
+            enemy.rect.y = win_height - enemy.rect.height
+
+        # Handle collision between player and enemy
+        if player_img.get_rect().colliderect(enemy.rect):
+            print("ow, my nuts")
+
+        # Handle collision between bullet and enemy
+        for bullet in bullets:
+            if enemy.rect.colliderect(bullet["rect"]):
+                bullets.remove(bullet)
+                enemies.remove(enemy)
+                break
+
+    # Update the camera position
     camera_x = player_x - camera_width/2
     camera_y = player_y - camera_height/2
-
-    # Limit the camera position to prevent going outside the background image
-    if camera_x < 0:
-        camera_x = 0
-    elif camera_x > bg.get_width() - camera_width:
-        camera_x = bg.get_width() - camera_width
-    if camera_y < 0:
-        camera_y = 0
-    elif camera_y > bg.get_height() - camera_height:
-        camera_y = bg.get_height() - camera_height
-
-# Update the background position based on the camera position
-    bg_x = -camera_x
-    bg_y = -camera_y
-
-# Redraw the screen
-    win.blit(bg, (bg_x, bg_y))
-    win.blit(enemy_img, (enemy_x, enemy_y))
+    # Draw the background and sprites to the screen
+    win.blit(bg, (bg_x - camera_x, bg_y - camera_y))
     win.blit(player_img, (player_x - camera_x, player_y - camera_y))
+
+    # Draw the enemies to the screen
+    for enemy in enemies:
+        win.blit(enemy.img, (enemy.rect.x - camera_x, enemy.rect.y - camera_y))
+
+    # Draw the bullets to the screen
+    for bullet in bullets:
+        bullet_img = pg.image.load("bullet.png")
+        win.blit(bullet_img, (bullet["rect"].x - camera_x, bullet["rect"].y - camera_y))
+
+
+    # Update the display
     pg.display.update()
+
 pg.quit()
